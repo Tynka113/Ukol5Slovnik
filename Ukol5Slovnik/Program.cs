a15262eb2d8ca5fd3f6a35c9527e5e6f4150f87e
Tynka113 <macha@DESKTOP-TMSSHNL>
2023-11-08 21:24:36 +0100

    Add project files.

---
+++ Ukol5Slovnik/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+/*
+ * Zadání:
+Vytvoř si skutečně slovník. 
+Tedy aplikaci kde přidáte slovo a jeho překlad (případně i více překladů). 
+
+Možné operace už jsou na vás, 
+ale bylo by vhodné moct v takové aplikaci si vše uložit a načíst ze souboru, 
+vyhledat apod.
+*/
